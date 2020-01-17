4e4fd3a73daea7c72d8d94093ab217864c55321f
Alex Rothenberg <alexroth96@gmail.com>
2020-01-17 18:05:48 -0500

    add more interesting svgs and fix responsiveness

--- src/components/services/servicesArray.js
+++ src/components/services/servicesArray.js
@@ -1,28 +1,23 @@
-import React from "react";
-import { FiVideo, FiUsers } from "react-icons/fi";
-import { TiDeviceLaptop } from "react-icons/ti";
+import { readingSvg, mathSvg, writingSvg } from './servicesstyles/servicesSvgs';
+
 export const services = [
-  {
-    icon: <TiDeviceLaptop />,
-    service: "English",
-    list: [
-      "Meet Online at your convenience",
-      "Virtually connect with Zoom",
-      "Instant feedback"
-    ]
-  },
-  {
-    icon: <FiVideo />,
-    service: "Writing",
-    list: [
-      "Access to personalized tutoring videos",
-      "Tutoring in the comfort of your own home",
-      "Video playback ability"
-    ]
-  },
-  {
-    icon: <FiUsers />,
-    service: "Math",
-    list: ["One on one sessions", "Meet in person", "Instant feedback"]
-  }
+	{
+		icon: readingSvg,
+		service: 'English',
+		list: ['Meet Online at your convenience', 'Virtually connect with Zoom', 'Instant feedback'],
+	},
+	{
+		icon: writingSvg,
+		service: 'Writing',
+		list: [
+			'Access to personalized tutoring videos',
+			'Tutoring in the comfort of your own home',
+			'Video playback ability',
+		],
+	},
+	{
+		icon: mathSvg,
+		service: 'Math',
+		list: ['One on one sessions', 'Meet in person', 'Instant feedback'],
+	},
 ];

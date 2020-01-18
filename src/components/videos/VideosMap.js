bdd876f7a0371b381ba6c77e82e52aba3ada4e36
Alex Roth <alexroth96@gmail.com>
2020-01-18 16:46:30 -0500

    video design page

--- src/components/videos/VideosMap.js
+++ src/components/videos/VideosMap.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { videos } from './videos';
-import videoStyles from './videostyles/VideosMap.module.scss';
+import React from "react";
+import { videos } from "./videos";
+import videoStyles from "./videostyles/VideosMap.module.scss";
 
 export const VideosMap = () => {
-	const videoMap = videos.map((video, i) => {
-		return (
-			<div className={videoStyles.video__slider} key={i * Math.random() * 100}>
-				<div className={videoStyles.slider__heading}>
-					<h3>{video.category}</h3>
-				</div>
-				<div className={videoStyles.slider__container}>
-					{video.videos.map((video, i) => (
-						<div className={videoStyles.video} key={i}>
-							{video}
-						</div>
-					))}
-				</div>
-			</div>
-		);
-	});
+  const videoMap = videos.map((video, i) => {
+    return (
+      <div className={videoStyles.video__slider} key={i * Math.random() * 100}>
+        <div className={videoStyles.slider__heading}>
+          <h3>{video.category}</h3>
+        </div>
+        <div className={videoStyles.slider__container}>
+          {video.videos.map((video, i) => (
+            <div className={videoStyles.video} key={i}>
+              {video}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  });
 
-	return <>{videoMap}</>;
+  return <div className={videoStyles.videos}>{videoMap}</div>;
 };

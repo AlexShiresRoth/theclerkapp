e59762449a9de47e3407b073920f2e319475dc57
Alex Roth <alexroth96@gmail.com>
2020-01-19 16:02:50 -0500

    layout videos page skeleton

--- src/components/videos/VideosMap.js
+++ src/components/videos/VideosMap.js
@@ -5,11 +5,14 @@ import videoStyles from "./videostyles/VideosMap.module.scss";
 export const VideosMap = () => {
   const videoMap = videos.map((video, i) => {
     return (
-      <div className={videoStyles.video__slider} key={i * Math.random() * 100}>
+      <div className={videoStyles.video__container}>
         <div className={videoStyles.slider__heading}>
           <h3>{video.category}</h3>
         </div>
-        <div className={videoStyles.slider__container}>
+        <div
+          className={videoStyles.video__slider}
+          key={i * Math.random() * 100}
+        >
           {video.videos.map((video, i) => (
             <div className={videoStyles.video} key={i}>
               {video}

0e7766ed646e43e1c606de455099d5f728837c37
Alex Roth <alexroth96@gmail.com>
2020-01-21 12:26:03 -0500

    fix heading sizes

--- src/components/videos/VideosHeader.js
+++ src/components/videos/VideosHeader.js
@@ -6,7 +6,7 @@ const VideosHeader = props => {
     <header className={videoStyles.header}>
       <div className={videoStyles.videos__container}>
         <div className={videoStyles.title__container}>
-          <h1>Tutorial Videos</h1>
+          <h1>Videos</h1>
           <p>
             Practice your skills, or learn something new with a video tutorial.
           </p>

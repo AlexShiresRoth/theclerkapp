bc693b69bd6777df99b717fe37655e2afdf141d2
Alex Rothenberg <alexroth96@gmail.com>
2020-03-12 12:02:30 -0400

    add in videos

--- src/components/videos/VideosMap.js
+++ src/components/videos/VideosMap.js
@@ -1,33 +1,31 @@
-import React from "react";
-import { videos } from "./videos";
-import videoStyles from "./videostyles/VideosMap.module.scss";
+import React from 'react';
+import { videos } from './videos';
+import videoStyles from './videostyles/VideosMap.module.scss';
 
 export const VideosMap = () => {
-  const videoMap = videos.map((video, i) => {
-    return (
-      <div className={videoStyles.video__container}>
-        <div className={videoStyles.slider__heading}>
-          <h3>{video.category}</h3>
-        </div>
-        <div
-          className={videoStyles.video__slider}
-          key={i * Math.random() * 100}
-        >
-          {video.videos.map((video, i) => (
-            <div className={videoStyles.video} key={i}>
-              {video}
-            </div>
-          ))}
-          <iframe
-            src="https://www.showme.com/sma/embed/?s=WyBexPM"
-            width="578"
-            height="433"
-            frameborder="0"
-          ></iframe>
-        </div>
-      </div>
-    );
-  });
+	const videoMap = videos.map((video, i) => {
+		return (
+			<div className={videoStyles.video__container}>
+				<div className={videoStyles.slider__heading}>
+					<h3>{video.category}</h3>
+				</div>
+				<div className={videoStyles.video__slider} key={i * Math.random() * 100}>
+					{video.videos.map((video, i) => (
+						<div className={videoStyles.video} key={i}>
+							<video src={video.src} type="video/mp4" controls>
+								Video Not Supported
+							</video>
+							<div className={videoStyles.caption}>
+								<a href={video.url} target="_blank" rel="noopener noreferrer">
+									{video.title}
+								</a>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	});
 
-  return <div className={videoStyles.container}>{videoMap}</div>;
+	return <div className={videoStyles.container}>{videoMap}</div>;
 };
--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -1,8 +1,29 @@
-import React from "react";
-
 export const videos = [
-  { category: "Math", videos: ["one", "two", "three"] },
-  { category: "English", videos: ["one", "two", "three"] },
-  { category: "Writing", videos: ["one", "two", "three"] },
-  { category: "SAT prep", videos: ["one", "two", "three"] }
+	{
+		category: 'Math',
+		videos: [
+			{
+				title: 'SAT Math #1 Equivelant Expressions',
+				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/wUcSNgu_ShowMe.mp4',
+				url: 'https://www.showme.com/sh/?h=wUcSNgu',
+			},
+			{
+				title: 'Algebra 2/ Trigonometry #1 Finding Inverse Functions',
+				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/BwdPxey_ShowMe.mp4',
+				url: 'https://www.showme.com/sh/?h=BwdPxey',
+			},
+		],
+	},
+	{
+		category: 'English',
+		videos: [],
+	},
+	{
+		category: 'Writing',
+		videos: [],
+	},
+	{
+		category: 'SAT prep',
+		videos: [],
+	},
 ];

9404ee5825836a3e30acbbf2674f695d13f0bc84
Alex Rothenberg <alexroth96@gmail.com>
2020-03-20 14:44:27 -0400

    add more videos

--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -1,11 +1,26 @@
 export const videos = [
 	{
-		category: 'Math',
+		category: 'Algebra',
 		videos: [
 			{
-				title: 'SAT Math #1 Equivelant Expressions',
-				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/wUcSNgu_ShowMe.mp4',
-				url: 'https://www.showme.com/sh/?h=wUcSNgu',
+				title: 'Algebra 1 tape 1 June 2019 regents #1-5',
+				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/20/10/V3mq7fs_ShowMe.mp4',
+				url: 'https://www.showme.com/sh?h=V3mq7fs',
+			},
+			{
+				title: 'Algebra 1 tape 2 # 6-10',
+				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/20/11/xKAY88W_ShowMe.mp4',
+				url: 'https://www.showme.com/sh?h=xKAY88W',
+			},
+			{
+				title: 'Algebra 1 tape 3 June 2019 regents #11-15',
+				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/20/11/j02XRse_ShowMe.mp4',
+				url: 'https://www.showme.com/sh?h=j02XRse',
+			},
+			{
+				title: 'Algebra 1 tape 4 June 2019 regents #16-20',
+				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/20/12/oOiP9xQ_ShowMe.mp4',
+				url: 'https://www.showme.com/sh?h=oOiP9xQ',
 			},
 			{
 				title: 'Algebra 2/ Trigonometry #1 Finding Inverse Functions',
@@ -24,6 +39,12 @@ export const videos = [
 	},
 	{
 		category: 'SAT prep',
-		videos: [],
+		videos: [
+			{
+				title: 'SAT Math #1 Equivelant Expressions',
+				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/wUcSNgu_ShowMe.mp4',
+				url: 'https://www.showme.com/sh/?h=wUcSNgu',
+			},
+		],
 	},
 ];

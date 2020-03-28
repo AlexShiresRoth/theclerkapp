9b64a297495a93f3cbe91040df2f65194075640f
Alex Roth <alexroth96@gmail.com>
2020-03-28 16:22:29 -0400

    idk what commit this is

--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -1,29 +1,32 @@
 export const videos = [
-	{
-		category: 'Math',
-		videos: [
-			{
-				title: 'SAT Math #1 Equivelant Expressions',
-				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/wUcSNgu_ShowMe.mp4',
-				url: 'https://www.showme.com/sh/?h=wUcSNgu',
-			},
-			{
-				title: 'Algebra 2/ Trigonometry #1 Finding Inverse Functions',
-				src: 'https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/BwdPxey_ShowMe.mp4',
-				url: 'https://www.showme.com/sh/?h=BwdPxey',
-			},
-		],
-	},
-	{
-		category: 'English',
-		videos: [],
-	},
-	{
-		category: 'Writing',
-		videos: [],
-	},
-	{
-		category: 'SAT prep',
-		videos: [],
-	},
+  {
+    category: "Algebra",
+    videos: [
+      {
+        title: "Algebra 2/ Trigonometry #1 Finding Inverse Functions",
+        src:
+          "https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/BwdPxey_ShowMe.mp4",
+        url: "https://www.showme.com/sh/?h=BwdPxey"
+      }
+    ]
+  },
+  {
+    category: "English",
+    videos: []
+  },
+  {
+    category: "Writing",
+    videos: []
+  },
+  {
+    category: "SAT prep",
+    videos: [
+      {
+        title: "SAT Math #1 Equivelant Expressions",
+        src:
+          "https://s3.amazonaws.com/media1.showme.com/2020/03/11/12/wUcSNgu_ShowMe.mp4",
+        url: "https://www.showme.com/sh/?h=wUcSNgu"
+      }
+    ]
+  }
 ];
